c7ed7daf6538e018e721e670d51f972edc88e443
josephus <otejerejosephus@gmail.com>
2025-05-06 20:41:27 +0100

    completed the color prototype in main

--- main.py
+++ main.py
@@ -1,41 +1,66 @@
 from PIL import Image
 
 def main():
-    print("snow")
-    img = Image.open(r"test.png")
-    img_grey = img.convert("I")
-    resized_image = logic_for_resize(img_grey)
-    ans = convert(resized_image)
+    img = Image.open(r"sample/serpent.png")
+    resized_image = logic_for_resize(img)
+    ans = convert(resized_image, False)
     print(ans)
     
 
 
     #resized_image.show()
 
-def convert(image):
+def _get_ansi_color(char, rgb):
+        """
+        Return the ansii string to display colored characters in the terminal if true color is supported.
+        """
+        if type(rgb) == int:
+            r = g = b = rgb
+        else:
+            r, g, b = rgb
+        return f"\x1b[38;2;{r};{g};{b}m{char}\x1b[0m"
+
+def convert(image, color=False):
     width, _ = image.size
+    img_grey = image.convert("I")
+    img_color_data = image.convert("RGB")
+
     char_list = ["@", "J", "D", "%", "*", "P", "+", "Y", "$", ",", "."]
-    pixels = image.getdata()
-   
-#the data contained in the image contains pixel values ranging from 0 to 255; representing
-#darkest at 0 and light at 255.
-
+    """
+    the data contained in the image contains pixel values ranging from 0 to 255; representing
+    darkest at 0 and light at 255.
+    """
+    pixels = img_grey.getdata()
+    if color:
+        color_pixels = img_color_data.getdata()
+        #new_color_pixels = [char_list[pixel//25] for pixel in color_pixels]
+    """
+    here we are addressing the 11 characters from the list arr[0] for the thickest characters
+    and arr[10] for lightest characters.
+    """
     new_pixels = [char_list[pixel//25] for pixel in pixels]
-#here we are addressing the 11 characters from the list arr[0] for the thickest characters
-#and arr[10] for lightest characters.
 
+    
+    
     new_pixels = ''.join(new_pixels)
     new_pixels_count = len(new_pixels)
+    """
+    split string of chars into multiple strings of length equal to width and create a list
+    """
 
-# split string of chars into multiple strings of length equal to width and create a list
     ascii_image = []
     for i in range(0, new_pixels_count, width):
-        ascii_image.append(new_pixels[i:i+width])
+        if color:
+            line = ""
+            for letter in range(i, i+width):
+                color_char = _get_ansi_color(new_pixels[letter], color_pixels[letter])
+                line += color_char
+            ascii_image.append(line)
+        else:
+            ascii_image.append(new_pixels[i:i+width])
+    
     ascii_image_f = ("\n").join(ascii_image)
 
-# write to a text file.
-    f = open("sample_ascii_image.txt", "w")
-    f.write(ascii_image_f)
 
     return ascii_image_f
 
@@ -43,14 +68,6 @@ def convert(image):
 
 
 
-
-"""ascii_image = "\n".join(ascii_image)
-print(ascii_image)
- 
-
-with open("sample_ascii_image.txt", "w") as f:
- f.write(ascii_image)
-"""
 
 def logic_for_resize(image):
     width, height = image.size
